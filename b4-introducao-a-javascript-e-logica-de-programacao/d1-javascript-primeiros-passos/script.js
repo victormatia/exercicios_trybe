ff8fd68ecd94418e0748e1f4c9fbee0939c4bf8a
Victor Matias <fvictor1705@gmail.com>
2022-05-14 11:36:08 -0300

    Resolve questões 4 e 5.

--- b4-introducao-a-javascript-e-logica-de-programacao/d1-javascript-primeiros-passos/script.js
+++ b4-introducao-a-javascript-e-logica-de-programacao/d1-javascript-primeiros-passos/script.js
@@ -44,3 +44,35 @@
 //     console.log(n3);
 // }
 
+//--------------------------
+
+// // 4. Faça um programa que, dado um valor definido numa constante, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
+
+// const n = 100;
+
+// if (n > 0) {
+//     console.log("A constante 'n' é positiva!");
+// } else if (n < 0) {
+//     console.log("A constante 'n' é negativa!");
+// } else {
+//     console.log("A constante 'n' é: 0");
+// }
+
+
+// 5.🚀 Faça um programa que defina três constantes com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false, caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.
+
+const a1 = 45;
+const a2 = 90;
+const a3 = 45;
+
+if ((a1 === a2 && a1 === a3 && a2 === a3) && (a1 + a2 + a3 === 180)) {
+   console.log(((a1 === a2 && a1 === a3 && a2 === a3) && (a1 + a2 + a3 === 180)) + ', O triângulo é equilátero.'); 
+} else if ((a1 === 90 || a2 === 90 || a3 === 90) && (a1 + a2 + a3 === 180)) {
+    console.log(((a1 === 90 || a2 === 90 || a3 === 90) && (a1 + a2 + a3 === 180)) + ', O triângulo é retângulo.');
+} else if (a1 !== a2 && a1 !== a3 && a2 !== a3 && (a1 + a2 + a3 === 180)) {
+    console.log((a1 !== a2 && a1 !== a3 && a2 !== a3 && (a1 + a2 + a3 === 180)) + ', O triângulo é escaleno.');
+} else {
+    console.log("Erro!")
+}   // podemos adidionar varias outras consicionais para comportar os 6 tipos de triangulos existentes, mas a logica e a mesma.
+
+
