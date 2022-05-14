379d6002d269008882b199bb79c05eba7e92a1f7
Victor Matias <fvictor1705@gmail.com>
2022-05-14 13:52:05 -0300

    Resolve a questão 6.

--- b4-introducao-a-javascript-e-logica-de-programacao/d1-javascript-primeiros-passos/script.js
+++ b4-introducao-a-javascript-e-logica-de-programacao/d1-javascript-primeiros-passos/script.js
@@ -1,4 +1,4 @@
-// 1.Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b, definidas no começo com os valores que serão operados. Faça programas para:
+// // 1.Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b, definidas no começo com os valores que serão operados. Faça programas para:
 // Adição (a + b)
 // Subtração (a - b)
 // Multiplicação (a * b)
@@ -16,7 +16,7 @@
 
 //--------------------------
 
-// 2.Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados.
+// // 2.Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados.
 
 // const n1 = 57;
 // const n2 = 2;
@@ -29,7 +29,7 @@
 
 //--------------------------
 
-// 3.Faça um programa que retorne o maior de três números. Defina no começo do programa três constantes com os valores que serão comparados.
+// // 3.Faça um programa que retorne o maior de três números. Defina no começo do programa três constantes com os valores que serão comparados.
 
 
 // const n1 = 57;
@@ -58,21 +58,87 @@
 //     console.log("A constante 'n' é: 0");
 // }
 
+//--------------------------
 
-// 5.🚀 Faça um programa que defina três constantes com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false, caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.
+// // 5.🚀 Faça um programa que defina três constantes com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false, caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.
 
-const a1 = 45;
-const a2 = 90;
-const a3 = 45;
+// const a1 = 45;
+// const a2 = 90;
+// const a3 = 45;
 
-if ((a1 === a2 && a1 === a3 && a2 === a3) && (a1 + a2 + a3 === 180)) {
-   console.log(((a1 === a2 && a1 === a3 && a2 === a3) && (a1 + a2 + a3 === 180)) + ', O triângulo é equilátero.'); 
-} else if ((a1 === 90 || a2 === 90 || a3 === 90) && (a1 + a2 + a3 === 180)) {
-    console.log(((a1 === 90 || a2 === 90 || a3 === 90) && (a1 + a2 + a3 === 180)) + ', O triângulo é retângulo.');
-} else if (a1 !== a2 && a1 !== a3 && a2 !== a3 && (a1 + a2 + a3 === 180)) {
-    console.log((a1 !== a2 && a1 !== a3 && a2 !== a3 && (a1 + a2 + a3 === 180)) + ', O triângulo é escaleno.');
+// if ((a1 === a2 && a1 === a3 && a2 === a3) && (a1 + a2 + a3 === 180)) {
+//    console.log(((a1 === a2 && a1 === a3 && a2 === a3) && (a1 + a2 + a3 === 180)) + ', O triângulo é equilátero.'); 
+// } else if ((a1 === 90 || a2 === 90 || a3 === 90) && (a1 + a2 + a3 === 180)) {
+//     console.log(((a1 === 90 || a2 === 90 || a3 === 90) && (a1 + a2 + a3 === 180)) + ', O triângulo é retângulo.');
+// } else if (a1 !== a2 && a1 !== a3 && a2 !== a3 && (a1 + a2 + a3 === 180)) {
+//     console.log((a1 !== a2 && a1 !== a3 && a2 !== a3 && (a1 + a2 + a3 === 180)) + ', O triângulo é escaleno.');
+// } else {
+//     console.log("Erro!")
+// }   // podemos adidionar varias outras consicionais para comportar os 6 tipos de triangulos existentes, mas a logica e a mesma.
+
+//--------------------------
+
+// 6.Escreva um programa que receba o nome de uma peça de xadrez e retorne os movimentos que ela faz.
+// Como desafio, faça o programa funcionar tanto se receber o nome de uma peça com letras maiúsculas quanto com letras minúsculas, sem aumentar a quantidade de condicionais.
+// Como dica, você pode pesquisar uma função que faz uma string ficar com todas as letras minúsculas (lower case).
+// Se a peça passada for inválida, o programa deve retornar uma mensagem de erro.
+// Exemplo: bishop (bispo) -> diagonals (diagonais)
+
+// Adicione a peça aqui ↓↓↓
+
+let peca ="Galinha";
+
+// Obs.: não utilize acentuações.
+//------------------ 
+
+
+//----- Não altere nada abaixo dessa linha ------
+
+let evitadorDeErro = peca.toLowerCase() // variavel.toLowerCase converte o valor de uma string para minúsculo
+
+let movimentos = ['em todas as direcoes um casa por rodada', 'em diagonal uma casa por rodada', ' para esquerda', ' para a direita', ' para frente', ' para tras', 'em L uma vez por rodada', 'em todas as direcoes quantas casas quiser por rodada', 'para frente um casa por rodada']
+
+
+if (evitadorDeErro === "rei") {
+    for (let i = 0; i < movimentos.length; i += 1) {
+        if (movimentos[i] === 'em todas as direcoes um casa por rodada') {
+            console.log("Sua peça se move " + movimentos[i]);
+        } 
+    } 
+} else if (evitadorDeErro === "rainha") {
+    for (let i = 0; i < movimentos.length; i += 1) {
+        if (movimentos[i] === 'em todas as direcoes quantas casas quiser por rodada') {
+            console.log("Sua peça se move " + movimentos[i]);
+        } 
+    } 
+} else if (evitadorDeErro === "bispo") {
+    for (let i = 0; i < movimentos.length; i += 1) {
+        if (movimentos[i] === 'em diagonal uma casa por rodada') {
+            console.log("Sua peça se move " + movimentos[i]);
+        } 
+    }
+} else if (evitadorDeErro === "torre") {
+    let movimentosDaTorre = [];
+    for (let i = 0; i < movimentos.length; i += 1) {
+        if (movimentos[i] === ' para esquerda' || movimentos[i] === ' para tras' || movimentos[i] === ' para frente' || movimentos[i] === ' para a direita') {
+            movimentosDaTorre.push(movimentos[i]);
+        }
+    }
+    console.log("Sua peça se move" + movimentosDaTorre + ", uma casa por rodada.")
+} else if (evitadorDeErro === "cavalo") {
+    for (let i = 0; i < movimentos.length; i += 1) {
+        if (movimentos[i] === 'em L uma vez por rodada') {
+            console.log("Sua peça se move " + movimentos[i]);
+        } 
+    }
+} else if (evitadorDeErro === "peao") {
+    for (let i = 0; i < movimentos.length; i += 1) {
+        if (movimentos[i] === 'para frente um casa por rodada') {
+            console.log("Sua peça se move " + movimentos[i]);
+        } 
+    }
 } else {
-    console.log("Erro!")
-}   // podemos adidionar varias outras consicionais para comportar os 6 tipos de triangulos existentes, mas a logica e a mesma.
+    console.log("Peça inválida. Tente os valores: Rei, Rainha, Bispo, Torre, Cavalo ou Peao");
+}
 
-
+//--------------------------
